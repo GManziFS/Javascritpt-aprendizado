8b9188a7dc313087b35e7d4d40391995a8e2cabe
GManziFS <gabrielmanzifs@gmail.com>
2024-10-02 06:12:56 -0300

    just else if

---
+++ ex011/ex011.js
@@ -0,0 +1,17 @@
+var idade = 149
+if (idade < 12){
+    console.log('un bebe')
+    } else if(idade < 18) {
+        console.log('Menor de idade')
+        } else if (idade < 29) {
+            console.log('Jovem ainda')
+            } else if(idade < 50){
+                console.log('Adulto')
+                } else if(idade < 85) {
+                    console.log('Idoso')
+                    } else if(idade < 150){
+                        console.log('Mumia')
+                        }
+ 
+
+
